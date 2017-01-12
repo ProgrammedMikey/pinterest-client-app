04db90257d5359824d53454a9084a6d065698ccf
Michael DaSilva <michaeldasilva@Michaels-MacBook-Pro.local>
2017-01-11 20:42:39 -0500

    install express

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-// const port = process.env.port || 8080;
+const PORT = process.env.PORT || 8080;
 
 app.use(express.static(__dirname));
 
@@ -10,5 +10,5 @@ app.get('*', (req, res) => {
 });
 
 
-app.listen(process.env.port);
+app.listen(PORT);
 console.log('Express server is up');
